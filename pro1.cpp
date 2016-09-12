70e93b3d55eb176e46e1f3d8625b313f88d7d02e
Mikael Persson <mikaepe@kth.se>
2016-09-12 14:28:31 +0200

    updated factorial with long long int

--- pro1.cpp
+++ pro1.cpp
@@ -14,7 +14,7 @@
 
 double sinTaylor(int N, double x);
 double cosTaylor(int N, double x);
-int factorial(int K);
+long long int factorial(int K);
 
 
 // function definitions	::	::	::	::
@@ -24,7 +24,8 @@ using namespace std;
 int main(int argc, char *argv[]) {
   // main function, i.e. the program..
 
-  int K,fac;
+  int K;
+  long long int fac;
   cout << "K = ";
   cin >> K;
   fac = factorial(K);
@@ -33,14 +34,44 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-int factorial(int K) {
-  // returns factorial(K), e.g. factorial(3) = 1*2*3 = 6
+/*
 
-  int fac;
-  fac = 1;
+double sinTaylor(int N, double x) {
+  // returns value of N:th degree taylor polynomial for
+  // sin function evaluated at x
+  double sinT;
+
+  return sinT;
+}
+*/
+
+
+
+/*
+
+double horner(int a,double x) {
+
+  double h;
+  // Horners algorithm:
+  // p(x) = a0 + a1*x + ... + aN*x^N
+  // rewrite:
+  // p(x) = a0 + x*(a1 + x*(a2 + ... + x*(aN-1 + x*aN)))...)
+  // so we define a sequence:
+  // bN = aN; bN-1 = aN-1 + bN*x; ... ; b0 = a0 + b1*x = p(x)
+
+  return h;
+}
+*/
+
+
+long long int factorial(int K) {
+  // returns factorial(K), e.g. factorial(3) = 1*2*3 = 6
+  // seems to work only up to 20!
+
+  long long int fac = 1;
 
   for (int k = 1; k <= K; k++)
-    fac = fac*k;
+    fac *= k;
 
   return fac;
 }
