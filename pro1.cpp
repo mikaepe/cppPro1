c21dbfdad746823fd7d528b8dbfb3c944e859fef
Mikael Persson <mikaepe@kth.se>
2016-09-12 13:38:59 +0200

    created pro1.cpp as test

---
+++ pro1.cpp
@@ -0,0 +1,13 @@
+/* Project 1, SF2565, KTH
+ *
+ */
+
+#include<iostream>
+
+int main(int argc, char *argv[]) {
+  // main function, i.e. the program..
+
+  std::cout << "hello" << endl;
+
+  return 0;
+}
