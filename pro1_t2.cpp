e9d556e5595f1c1e17522e7270dc6418db082dd3
Mikael Persson <mikaepe@kth.se>
2016-09-14 22:08:27 +0200

    added

---
+++ pro1_t2.cpp
@@ -0,0 +1,25 @@
+/* Project 1 - Task 2, SF2565, KTH, 2016
+ * Hanna Hultin & Mikael Persson
+ */
+
+
+// libraries	::	::	::	::	::
+
+#include<iostream>
+#include<cmath>
+
+// function declarations	::	::	::
+
+
+
+// function definitions	::	::	::	::
+
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+  // main function.
+
+  return 0;
+}
+
