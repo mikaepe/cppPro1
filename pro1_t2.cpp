dc0dce34baf6c0df86e23eb303f91d34f7dd1496
Mikael Persson <mikaepe@kth.se>
2016-09-19 11:48:03 +0200

    recursive formula working

--- pro1_t2.cpp
+++ pro1_t2.cpp
@@ -12,6 +12,9 @@
 
 double f(double x);
 double iSimpson(double a, double b);
+double mid(double x, double y);
+double i2Simpson(double a, double b);
+double iRecursive(double a, double b, double tol);
 
 // function definitions	::	::	::	::
 
@@ -22,20 +25,36 @@ int main(int argc, char *argv[])
   // main function.
 
   double a = -1.0,b = 1.0,tol = 1e-2;
+  double I;
 
-  double x,y,I;
-
-  x = f(a);
-  y = f(b);
-  I = iSimpson(a,b);
+  I = iRecursive(a,b,tol);
 
   cout << I << endl;
   cout << "Matlab: I = 2.500809110336167" << endl;
 
-  cout << x << endl;
-  cout << y << endl;
   cout << tol << endl;
   return 0;
+}
+
+double iRecursive(double a, double b, double tol)
+{
+  double I = iSimpson(a,b);
+  double I2 = i2Simpson(a,b);
+  double errest = abs(I-I2);
+  if (errest < 15*tol)
+    return I2;
+  return iRecursive(a, mid(a,b),0.5*tol) + iRecursive(mid(a,b),b,0.5*tol);
+}
+
+double i2Simpson(double a, double b)
+{
+  return iSimpson(a,mid(a,b)) + iSimpson(mid(a,b),b);
+}
+
+double mid(double x, double y)
+{
+  // returns the midpoint of x & y
+  return 0.5*(x+y);
 }
 
 double iSimpson(double a, double b)
