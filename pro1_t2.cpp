26781a1919203d2c04320ca48db50ed01445f054
Hanna Hultin <Hanna Hultin>
2016-09-27 15:06:38 +0100

    changed comments on task 2

--- pro1_t2.cpp
+++ pro1_t2.cpp
@@ -15,7 +15,6 @@ double f(double x);
 double iSimpson(double a, double b);
 double mid(double x, double y);
 double i2Simpson(double a, double b);
-double iRecursive(double a, double b, double tol);
 double asi(double a, double b, double tol);
 
 // function definitions	::	::	::	::
@@ -26,28 +25,30 @@ using namespace std;
 int main(int argc, char *argv[]) 
 {
   double a = -1.0,b = 1.0,tol = 1e-8;
-  double I, I_wr;
-
-  I = iRecursive(a,b,tol);
-  I_wr = asi(a,b,tol);
+  double I;
+  
+  I = asi(a,b,tol);
 
   cout << fixed << setprecision(15);		// more decimals
-  cout << "With recursion: "<< I << endl;
-  cout << "Without recursion: " << I_wr << endl;
+  cout << "Without recursion: " << I << endl;
   cout << "Matlab: I = 2.500809110336167" << endl << flush;
 
   cout << "Tol: " << tol << endl;
   return 0;
 }
 
-/* Ser rekursionen som ett träd istället för rekursivt
- * Roten: node 1. 
- * Varje node k har (max) två barn:
- *  node 2*k (den vänstra) och node 2*k+1 (den högra)
- * I den vänstra ändras b till mid(a,b) och i högra blir a mid(a,b)
- * Föräldern till en node fås av floor(node/2) om node!=1
+/* asi: Adaptive Simpson Integration between a and b with tolerance tol
+ * Use a tree instead of recursion
+ * The root is node number 1, and each node k has at most 2 children:
+ * - node 2k (the left one)
+ * - node 2k+1 (the right one)
+ * In the left child b is changed to mid(a,b) and tol=tol/2
+ * In the right child a is changed to mid(a,b) and tol=tol/2
+ * The parent of a node is given by floor(0.5*node) if node!=1
  */
-double asi(double a, double b, double tol) {
+
+double asi(double a, double b, double tol) 
+{
     double I = 0, I1, I2, errest;
     int node = 1;
 
@@ -55,31 +56,29 @@ double asi(double a, double b, double tol) {
         I1 = iSimpson(a, b);
         I2 = i2Simpson(a, b);
         errest = abs(I1 - I2);
-        if (errest < 15 * tol) {
+        if (errest < 15 * tol) { //if leaf
             I += I2;
-            /* Om ojämn node: 
-             *  Fortsätt upp till  1a jämna
-             *  För varje hopp upp från ojämn node: 
-             *     node = floor(0.5*node), byt tillbaks a
+            /* while uneven node: 
+             * go up until first even node
              */
             while (node % 2 != 0) {
                 if (node == 1) {
-                    return I; // return om vi är uppe i roten igen
+                    return I; // return if we are back in the root again
                 }
                 node = floor(0.5 * node);
                 a = 2 * a - b;
                 tol *= 2;
             }
-            /* Om jämn: 
-             *  En upp: node = 0.5*node, byt tillbaks b 
-             *  Gå till höger: node = 2*node +1, ändra a
+            /* first even node: 
+             * - go one node up
+             * - go to the right child 
              */
             node = 0.5 * node;
             b = 2 * b - a;
             node = 2 * node + 1;
             a = mid(a,b);
         }            
-        // Gå till vänster: node*=2, ändra b
+        // If not a leaf: go to the left child
         else {
             node *= 2;
             b = mid(a,b);
@@ -89,17 +88,7 @@ double asi(double a, double b, double tol) {
     return I;
 }
 
-double iRecursive(double a, double b, double tol)
-{
-  double I = iSimpson(a,b);
-  double I2 = i2Simpson(a,b);
-  double errest = abs(I-I2);
-  if (errest < 15*tol)
-    return I2;
-  return iRecursive(a, mid(a,b),0.5*tol) + iRecursive(mid(a,b),b,0.5*tol);
-}
-
-// Simpson of left half interval and right half interval
+// Simpson of left half interval and right half interval of (a,b)
 double i2Simpson(double a, double b)
 {
   return iSimpson(a,mid(a,b)) + iSimpson(mid(a,b),b);
@@ -111,13 +100,13 @@ double mid(double x, double y)
   return 0.5*(x+y);
 }
 
-// Simpson quadrature
+// Simpson quadrature between a and b
 double iSimpson(double a, double b)
 {
   return ((b-a)/6.0)*(f(a)+4.0*f(0.5*(a+b)) + f(b));
 }
 
-// The function to be integrated
+// The value of the function to be integrated in the point x
 double f(double x) 
 {
   return 1.0+sin(exp(3*x));
