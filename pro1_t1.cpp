cb3b570814a56576ee356e75d2c0af58824a3798
Mikael Persson <mikaepe@kth.se>
2016-09-27 15:59:52 +0200

    pro1_t1.cpp complete

--- pro1_t1.cpp
+++ pro1_t1.cpp
@@ -1,9 +1,7 @@
-/* Project 1 - Task 1, SF2565, KTH, 2016
- * comparison of taylor series for sin(x) and cos(x)
- * by cmath functions sin(x) and cos(x)
- * Hanna Hultin & Mikael Persson
+/* Project 1 - Task 1, SF2565, KTH, 2016. Comparison of 
+ * taylor series for sin(x) and cos(x) by cmath functions 
+ * sin(x) and cos(x). Hanna Hultin & Mikael Persson
  */
-
 
 // libraries	::	::	::	::	::
 
@@ -11,13 +9,10 @@
 #include<cmath>
 #include<iomanip>
 
-
 // function declarations	::	::	::
 
 double sinTaylor(int N, double x);
 double cosTaylor(int N, double x);
-void errorBound(int N,double x,double sx,double sTx,double cx,double cTx);
-
 
 // function definitions	::	::	::	::
 
@@ -32,20 +27,25 @@ int main(int argc, char *argv[])
 {
   double sx, sTx, cx, cTx;	// sinx/cosx from cmath & Taylor polyn.
   double x; int N;
-  cout << "x = "; cin >> x;	// Promt user for x and N
-  cout << "N = "; cin >> N;
-  cout << fixed << setprecision(15);	// Print more decimals
+  cout << "x = "; cin >> x;	// Promt user for x
+  cout << "N = "; cin >> N;	// Promt user for N
+  cout << fixed << setprecision(15);	// To print more decimals
+
+  // Compute the Taylor-polynomials and values from cmath
   
   sx = sin(x);
-  cout << "cmath:   sin(x) = " << sx << endl;
   sTx = sinTaylor(N,x);
-  cout << "Taylor:  sin(x) = " << sTx << endl;
-  
   cx = cos(x);
-  cout << "cmath:   cos(x) = " << cx << endl;
   cTx = cosTaylor(N,x);
+  
+  // Print some results
+  cout << "cmath:   sin(x) = " << sx << endl;
+  cout << "Taylor:  sin(x) = " << sTx << endl;
+  cout << "cmath:   cos(x) = " << cx << endl;
   cout << "Taylor:  cos(x) = " << cTx << endl;
 
+
+  // Compute the errorterms etc
   double  sinTermN, cosTermN;
   sinTermN = sinTaylor(N+1,x) - sTx;
   cosTermN = cosTaylor(N+1,x) - cTx;
@@ -59,8 +59,7 @@ int main(int argc, char *argv[])
 }
 
 /* Function sinTaylor returns value of N:th degree taylor polynomial for
- * sin function evaluated at x. Horners algorithm is used to compute the series:
- *
+ * sin function evaluated at x. Horners algorithm is used to compute the series.
  * Note, some adjustments are made to cope with the fact that each
  * second term vanishes in the sin series.
  */
@@ -68,11 +67,11 @@ double sinTaylor(int N, double x)
 {
   double sinT;
   sinT = 1;
-  for (int i = N; i > 0; i--)
+  for (int i = N; i > 0; i--)	// Run the loop backwards
   {
     sinT = 1-x*x*sinT/(double)(2*i*(2*i+1));
   }
-  sinT = x*sinT;
+  sinT = x*sinT;		// to get 2n+1 instead of 2n
   return sinT;
 }
 
@@ -81,7 +80,6 @@ double sinTaylor(int N, double x)
  */
 double cosTaylor(int N, double x) 
 {
- 
   double cosT;
   cosT = 1;
   for (int i = N; i > 0; i--) // Analogous to sin
